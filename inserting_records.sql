c650e07a4c0feebfff2bd1d93848a2ca17ea30b4
Yohan Jeong <yjeong5126@gmail.com>
2020-11-18 15:49:01 -0800

    Update inserting_records.sql

--- inserting_records.sql
+++ inserting_records.sql
@@ -1,46 +1,46 @@
 
-load data infile '/ProgramData/MySQL/MySQL Server 8.0/Uploads/glassdoor_data_analyst/states.csv' 
+load data infile '/states.csv' 
 into table states 
 fields terminated by ','  
 lines terminated by '\r\n' 
 (state);
 
-load data infile '/ProgramData/MySQL/MySQL Server 8.0/Uploads/glassdoor_data_analyst/sectors.csv' 
+load data infile '/sectors.csv' 
 into table sectors 
 fields terminated by ',' 
 optionally enclosed by '"' 
 lines terminated by '\r\n' 
 (sector);
 
-load data infile '/ProgramData/MySQL/MySQL Server 8.0/Uploads/glassdoor_data_analyst/revenues.csv' 
+load data infile '/revenues.csv' 
 into table revenues 
 fields terminated by ',' 
 optionally enclosed by '"' 
 lines terminated by '\r\n' 
 (revenue);
 
-load data infile '/ProgramData/MySQL/MySQL Server 8.0/Uploads/glassdoor_data_analyst/sizes.csv' 
+load data infile '/sizes.csv' 
 into table sizes 
 fields terminated by ',' 
 optionally enclosed by '"' 
 lines terminated by '\r\n' 
 (size);
 
-load data infile '/ProgramData/MySQL/MySQL Server 8.0/Uploads/glassdoor_data_analyst/skills.csv' 
+load data infile '/skills.csv' 
 into table skills 
 fields terminated by ',' 
 optionally enclosed by '"' 
 lines terminated by '\r\n' 
 (skill);
 
-load data infile '/ProgramData/MySQL/MySQL Server 8.0/Uploads/glassdoor_data_analyst/industries.csv' 
+load data infile '/industries.csv' 
 into table industries 
 fields terminated by ',' 
 optionally enclosed by '"' 
 lines terminated by '\r\n' 
 (industry, sectorID);
 
-load data infile '/ProgramData/MySQL/MySQL Server 8.0/Uploads/glassdoor_data_analyst/companies.csv' 
+load data infile '/companies.csv' 
 into table companies 
 fields terminated by ',' 
 optionally enclosed by '"' 
@@ -48,14 +48,14 @@ lines terminated by '\r\n'
 (companyName, sizeID, revenueID, industryID, @rating)
 SET rating = IF(char_length(trim(@rating)) = 0, NULL, @rating);
 
-load data infile '/ProgramData/MySQL/MySQL Server 8.0/Uploads/glassdoor_data_analyst/jobpostings.csv' 
+load data infile '/jobpostings.csv' 
 into table jobpostings 
 fields terminated by ',' 
 optionally enclosed by '"' 
 lines terminated by '\r\n' 
 (title, companyID, lower_salary, upper_salary, job_description, city, stateID);
 
-load data infile '/ProgramData/MySQL/MySQL Server 8.0/Uploads/glassdoor_data_analyst/jobpostings_skills.csv' 
+load data infile '/jobpostings_skills.csv' 
 into table jobpostings_skills 
 fields terminated by ',' 
 lines terminated by '\r\n' 
